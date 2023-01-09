2aeb2563be7968e40492d189d27ae1f8338532d1
GERALD ADDEY <geraldaddey@yahoo.com>
2023-01-09 10:40:37 +0000

    fix routes to show and edit friends

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  get 'home/about'
+  get 'home/about' 
+
+ 
 
 
   # Defines the root path route ("/")
